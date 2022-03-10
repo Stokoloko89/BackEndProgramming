0b53d88341dbb3922b7cc1bd9b2cb32aec35fcb2
Samson <samsonqina@gmail.com>
2022-03-10 16:20:55 +0200

    MongoDB node phonebook

---
+++ mongo.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://stokoloko_89:${password}@cluster0.s5hlk.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+});
+
+person.save().then((result) => {
+  console.log("person saved!");
+  mongoose.connection.close();
+});
